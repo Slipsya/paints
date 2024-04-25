e72de4748c6d46486ab5dfd91368eb70852cac24
Slipsya <153213871+Slipsya@users.noreply.github.com>
2024-04-25 21:03:59 +0300

    Create cart.js

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+
+document.querySelector('.add-to-cart').addEventListener('click', function() {
+    alert('Товар добавлен в корзину');
+});
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
